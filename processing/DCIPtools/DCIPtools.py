11b189c8c8acfb874ce1a1a987cc41826a010683
Unknown <john.kuttai@gmail.com>
2018-10-30 15:20:58 -0700

    Fixed more syntax errors
    fixed syntax errors... did more of a test

--- processing/DCIPtools/DCIPtools.py
+++ processing/DCIPtools/DCIPtools.py
@@ -1282,7 +1282,7 @@ class Jpatch:
                                       self.readings[k].Vdp[i].Rx2Elev - doff]
                         if ip_type is None:
                             if self.readings[k].Vdp[i].K < 0:
-                                data.append(self.readings[k].Vdp[i].Mx) * -1.0
+                                data.append(self.readings[k].Vdp[i].Mx * -1.0)
                             else:
                                 data.append(self.readings[k].Vdp[i].Mx)
                         elif ip_type == "decay":
